07332eff68145dc1613855917965ed11e4901945
Aamir Afridi <aamirafridi@gmail.com>
2016-03-14 17:16:25 +0400

    Comments linted

--- jquery.marquee.js
+++ jquery.marquee.js
@@ -13,7 +13,7 @@
                 playState = 'animation-play-state',
                 css3AnimationIsSupported = false,
 
-                //Private methods
+                // Private methods
                 _prefixedEvent = function(element, type, callback) {
                     var pfx = ["webkit", "moz", "MS", "o", ""];
                     for (var p = 0; p < pfx.length; p++) {
@@ -37,36 +37,36 @@
                     $this.timer = setTimeout(animate, o.delayBeforeStart);
                 },
 
-                //Public methods
+                // Public methods
                 methods = {
                     pause: function() {
                         if (css3AnimationIsSupported && o.allowCss3Support) {
                             $marqueeWrapper.css(playState, 'paused');
                         } else {
-                            //pause using pause plugin
+                            // pause using pause plugin
                             if ($.fn.pause) {
                                 $marqueeWrapper.pause();
                             }
                         }
-                        //save the status
+                        // save the status
                         $this.data('runningStatus', 'paused');
-                        //fire event
+                        // fire event
                         $this.trigger('paused');
                     },
 
                     resume: function() {
-                        //resume using css3
+                        // resume using css3
                         if (css3AnimationIsSupported && o.allowCss3Support) {
                             $marqueeWrapper.css(playState, 'running');
                         } else {
-                            //resume using pause plugin
+                            // resume using pause plugin
                             if ($.fn.resume) {
                                 $marqueeWrapper.resume();
                             }
                         }
-                        //save the status
+                        // save the status
                         $this.data('runningStatus', 'resumed');
-                        //fire event
+                        // fire event
                         $this.trigger('resumed');
                     },
 
@@ -75,19 +75,19 @@
                     },
 
                     destroy: function() {
-                        //Clear timer
+                        // Clear timer
                         clearTimeout($this.timer);
-                        //Unbind all events
+                        // Unbind all events
                         $this.find("*").andSelf().unbind();
-                        //Just unwrap the elements that has been added using this plugin
+                        // Just unwrap the elements that has been added using this plugin
                         $this.html($this.find('.js-marquee:first').html());
                     }
                 };
 
-            //Check for methods
+            // Check for methods
             if (typeof options === 'string') {
                 if ($.isFunction(methods[options])) {
-                    //Following two IF statements to support public methods
+                    // Following two IF statements to support public methods
                     if (!$marqueeWrapper) {
                         $marqueeWrapper = $this.find('.js-marquee-wrapper');
                     }
@@ -105,10 +105,10 @@
             var dataAttributes = {},
             attr;
             $.each(o, function(key, value) {
-                //Check if element has this data attribute
+                // Check if element has this data attribute
                 attr = $this.attr('data-' + key);
                 if (typeof attr !== 'undefined') {
-                    //Now check if value is boolean or not
+                    // Now check if value is boolean or not
                     switch (attr) {
                         case 'true':
                             attr = true;
@@ -121,19 +121,19 @@
                 }
             });
 
-            //since speed option is changed to duration, to support speed for those who are already using it
+            // since speed option is changed to duration, to support speed for those who are already using it
             o.duration = o.speed || o.duration;
 
-            //Shortcut to see if direction is upward or downward
+            // Shortcut to see if direction is upward or downward
             verticalDir = o.direction == 'up' || o.direction == 'down';
 
-            //no gap if not duplicated
+            // no gap if not duplicated
             o.gap = o.duplicated ? parseInt(o.gap) : 0;
 
-            //wrap inner content into a div
+            // wrap inner content into a div
             $this.wrapInner('<div class="js-marquee"></div>');
 
-            //Make copy of the element
+            // Make copy of the element
             var $el = $this.find('.js-marquee').css({
                 'margin-right': o.gap,
                 'float': 'left'
@@ -143,26 +143,26 @@
                 $el.clone(true).appendTo($this);
             }
 
-            //wrap both inner elements into one div
+            // wrap both inner elements into one div
             $this.wrapInner('<div style="width:100000px" class="js-marquee-wrapper"></div>');
 
-            //Save the reference of the wrapper
+            // Save the reference of the wrapper
             $marqueeWrapper = $this.find('.js-marquee-wrapper');
 
-            //If direction is up or down, get the height of main element
+            // If direction is up or down, get the height of main element
             if (verticalDir) {
                 var containerHeight = $this.height();
                 $marqueeWrapper.removeAttr('style');
                 $this.height(containerHeight);
 
-                //Change the CSS for js-marquee element
+                // Change the CSS for js-marquee element
                 $this.find('.js-marquee').css({
                     'float': 'none',
                     'margin-bottom': o.gap,
                     'margin-right': 0
                 });
 
-                //Remove bottom margin from 2nd element if duplicated
+                // Remove bottom margin from 2nd element if duplicated
                 if (o.duplicated) $this.find('.js-marquee:last').css({
                     'margin-bottom': 0
                 });
@@ -183,10 +183,10 @@
                 }
 
             } else {
-                //Save the width of the each element so we can use it in animation
+                // Save the width of the each element so we can use it in animation
                 elWidth = $this.find('.js-marquee:first').width() + o.gap;
 
-                //container width
+                // container width
                 containerWidth = $this.width();
 
                 // adjust the animation speed according to the text length
@@ -203,7 +203,7 @@
                 }
             }
 
-            //if duplicated then reduce the speed
+            // if duplicated then reduce the speed
             if (o.duplicated) {
                 o.duration = o.duration / 2;
             }
@@ -217,7 +217,7 @@
                     animationCss3Str = '',
                     keyframeString = '';
 
-                //Check css3 support
+                // Check css3 support
                 if (elm.style.animation) {
                     keyframeString = '@keyframes ' + animationName + ' ';
                     css3AnimationIsSupported = true;
@@ -249,7 +249,7 @@
                 $marqueeWrapper.css('margin-left', o.direction == 'left' ? containerWidth + 'px' : '-' + elWidth + 'px');
             };
 
-            //if duplicated option is set to true than position the wrapper
+            // if duplicated option is set to true than position the wrapper
             if (o.duplicated) {
                 if (verticalDir) {
                     if (o.startVisible) {
@@ -280,10 +280,10 @@
                 }
             }
 
-            //Animate recursive method
+            // Animate recursive method
             var animate = function() {
                 if (o.duplicated) {
-                    //When duplicated, the first loop will be scroll longer so double the duration
+                    // When duplicated, the first loop will be scroll longer so double the duration
                     if (loopCount === 1) {
                         o._originalDuration = o.duration;
                         if (verticalDir) {
@@ -291,16 +291,16 @@
                         } else {
                             o.duration = o.direction == 'left' ? o.duration + (containerWidth / ((elWidth) / o.duration)) : o.duration * 2;
                         }
-                        //Adjust the css3 animation as well
+                        // Adjust the css3 animation as well
                         if (animationCss3Str) {
                             animationCss3Str = animationName + ' ' + o.duration / 1000 + 's ' + o.delayBeforeStart / 1000 + 's ' + o.css3easing;
                         }
                         loopCount++;
                     }
-                    //On 2nd loop things back to normal, normal duration for the rest of animations
+                    // On 2nd loop things back to normal, normal duration for the rest of animations
                     else if (loopCount === 2) {
                         o.duration = o._originalDuration;
-                        //Adjust the css3 animation as well
+                        // Adjust the css3 animation as well
                         if (animationCss3Str) {
                             animationName = animationName + '0';
                             keyframeString = $.trim(keyframeString) + '0 ';
@@ -313,7 +313,7 @@
                 if (verticalDir) {
                     if (o.duplicated) {
 
-                        //Adjust the starting point of animation only when first loops finishes
+                        // Adjust the starting point of animation only when first loops finishes
                         if (loopCount > 2) {
                             $marqueeWrapper.css('margin-top', o.direction == 'up' ? 0 : '-' + elHeight + 'px');
                         }
@@ -324,7 +324,7 @@
                     } else if (o.startVisible) {
                         // This loop moves the marquee out of the container
                         if (loopCount === 2) {
-                            //Adjust the css3 animation as well
+                            // Adjust the css3 animation as well
                             if (animationCss3Str) {
                                 animationCss3Str = animationName + ' ' + o.duration / 1000 + 's ' + o.delayBeforeStart / 1000 + 's ' + o.css3easing;
                             }
@@ -335,7 +335,7 @@
                         } else if (loopCount === 3) {
                             // Set the duration for the animation that will run forever
                             o.duration = o._completeDuration;
-                            //Adjust the css3 animation as well
+                            // Adjust the css3 animation as well
                             if (animationCss3Str) {
                                     animationName = animationName + '0';
                                     keyframeString = $.trim(keyframeString) + '0 ';
@@ -352,7 +352,7 @@
                 } else {
                     if (o.duplicated) {
 
-                        //Adjust the starting point of animation only when first loops finishes
+                        // Adjust the starting point of animation only when first loops finishes
                         if (loopCount > 2) {
                             $marqueeWrapper.css('margin-left', o.direction == 'left' ? 0 : '-' + elWidth + 'px');
                         }
@@ -364,7 +364,7 @@
                     } else if (o.startVisible) {
                         // This loop moves the marquee out of the container
                         if (loopCount === 2) {
-                            //Adjust the css3 animation as well
+                            // Adjust the css3 animation as well
                             if (animationCss3Str) {
                                 animationCss3Str = animationName + ' ' + o.duration / 1000 + 's ' + o.delayBeforeStart / 1000 + 's ' + o.css3easing;
                             }
@@ -375,7 +375,7 @@
                         } else if (loopCount === 3) {
                             // Set the duration for the animation that will run forever
                             o.duration = o._completeDuration;
-                            //Adjust the css3 animation as well
+                            // Adjust the css3 animation as well
                             if (animationCss3Str) {
                                 animationName = animationName + '0';
                                 keyframeString = $.trim(keyframeString) + '0 ';
@@ -391,40 +391,40 @@
                     }
                 }
 
-                //fire event
+                // fire event
                 $this.trigger('beforeStarting');
 
-                //If css3 support is available than do it with css3, otherwise use jQuery as fallback
+                // If css3 support is available than do it with css3, otherwise use jQuery as fallback
                 if (css3AnimationIsSupported) {
-                    //Add css3 animation to the element
+                    // Add css3 animation to the element
                     $marqueeWrapper.css(animationString, animationCss3Str);
                     var keyframeCss = keyframeString + ' { 100%  ' + _objToString(animationCss) + '}',
                          $styles = $marqueeWrapper.find('style');
 
-                    //Now add the keyframe animation to the marquee element
+                    // Now add the keyframe animation to the marquee element
                     if ($styles.length !== 0) {
-                        //Bug fixed for jQuery 1.3.x - Instead of using .last(), use following
+                        // Bug fixed for jQuery 1.3.x - Instead of using .last(), use following
                         $styles.filter(":last").html(keyframeCss);
                     } else {
                         $marqueeWrapper.append('<style>' + keyframeCss + '</style>');
                     }
 
-                    //Animation iteration event
+                    // Animation iteration event
                     _prefixedEvent($marqueeWrapper[0], "AnimationIteration", function() {
                         $this.trigger('finished');
                     });
-                    //Animation stopped
+                    // Animation stopped
                     _prefixedEvent($marqueeWrapper[0], "AnimationEnd", function() {
                         animate();
                         $this.trigger('finished');
                     });
 
                 } else {
-                    //Start animating
+                    // Start animating
                     $marqueeWrapper.animate(animationCss, o.duration, o.easing, function() {
-                        //fire event
+                        // fire event
                         $this.trigger('finished');
-                        //animate again
+                        // animate again
                         if (o.pauseOnCycle) {
                             _startAnimationWithDelay();
                         } else {
@@ -432,11 +432,11 @@
                         }
                     });
                 }
-                //save the status
+                // save the status
                 $this.data('runningStatus', 'resumed');
             };
 
-            //bind pause and resume events
+            // bind pause and resume events
             $this.bind('pause', methods.pause);
             $this.bind('resume', methods.resume);
 
@@ -444,39 +444,39 @@
                 $this.bind('mouseenter mouseleave', methods.toggle);
             }
 
-            //If css3 animation is supported than call animate method at once
+            // If css3 animation is supported than call animate method at once
             if (css3AnimationIsSupported && o.allowCss3Support) {
                 animate();
             } else {
-                //Starts the recursive method
+                // Starts the recursive method
                 _startAnimationWithDelay();
             }
 
         });
-    }; //End of Plugin
+    }; // End of Plugin
     // Public: plugin defaults options
     $.fn.marquee.defaults = {
-        //If you wish to always animate using jQuery
+        // If you wish to always animate using jQuery
         allowCss3Support: true,
-        //works when allowCss3Support is set to true - for full list see http://www.w3.org/TR/2013/WD-css3-transitions-20131119/#transition-timing-function
+        // works when allowCss3Support is set to true - for full list see http://www.w3.org/TR/2013/WD-css3-transitions-20131119/#transition-timing-function
         css3easing: 'linear',
-        //requires jQuery easing plugin. Default is 'linear'
+        // requires jQuery easing plugin. Default is 'linear'
         easing: 'linear',
-        //pause time before the next animation turn in milliseconds
+        // pause time before the next animation turn in milliseconds
         delayBeforeStart: 1000,
-        //'left', 'right', 'up' or 'down'
+        // 'left', 'right', 'up' or 'down'
         direction: 'left',
-        //true or false - should the marquee be duplicated to show an effect of continues flow
+        // true or false - should the marquee be duplicated to show an effect of continues flow
         duplicated: false,
-        //speed in milliseconds of the marquee in milliseconds
+        // speed in milliseconds of the marquee in milliseconds
         duration: 5000,
-        //gap in pixels between the tickers
+        // gap in pixels between the tickers
         gap: 20,
-        //on cycle pause the marquee
+        // on cycle pause the marquee
         pauseOnCycle: false,
-        //on hover pause the marquee - using jQuery plugin https://github.com/tobia/Pause
+        // on hover pause the marquee - using jQuery plugin https://github.com/tobia/Pause
         pauseOnHover: false,
-        //the marquee is visible initially positioned next to the border towards it will be moving
+        // the marquee is visible initially positioned next to the border towards it will be moving
         startVisible: false
     };
 })(jQuery);
